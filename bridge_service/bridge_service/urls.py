2ee0dc62b54c238e6b03f458d7e76c844c83822b
zahrabateninia <zahrabatenin@gmail.com>
2024-11-17 03:01:08 +0330

    Setup the URL products/urls.py

--- bridge_service/bridge_service/urls.py
+++ bridge_service/bridge_service/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import ProductListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', ProductListView.as_view(), name='product-list'),
 ]
+
